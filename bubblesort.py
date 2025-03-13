15b2c50e4501ecd0edaa0a434298e89827f5a3e7
Nasivi Ramya Anjani <141706274+nasiviramya@users.noreply.github.com>
2025-03-13 11:33:01 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,9 +1,7 @@
-'''Num = str(input())
-print(int(num,17))'''
-'''for i in range(len(arr)):
+arr = [1,5,6,7,8]
+for i in range(len(arr)):
     for j in range(len(arr)-1):
         if arr[i] < arr[j]:
             arr[i],arr[j] = arr[j],arr[i]
 print(arr)
 
-n
